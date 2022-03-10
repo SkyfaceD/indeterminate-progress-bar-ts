a5e3d2365dc190ab747b65417966ea072381d741
Atabayev Muslim <m.atabayev@tamur.kz>
2022-03-10 11:59:07 +0600

    Remove and exclude builded 'out' directory from repository

--- out/extensions.js
+++
@@ -1,6 +0,0 @@
-String.prototype.replaceAt = function (idx, replacement) {
-    return (this.substring(0, idx) +
-        replacement +
-        this.substring(idx + replacement.length));
-};
-export {};
--- out/util/models.js
+++
@@ -1,7 +0,0 @@
-export class Holder {
-    constructor(bar, canvas, context = canvas.getContext('2d')) {
-        this.bar = bar;
-        this.canvas = canvas;
-        this.context = context;
-    }
-}
